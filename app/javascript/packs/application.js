06ba0cb45c31ac20f310a9176f01d7689d73e56e
unknown <hundred.industries@gmail.com>
2017-05-10 13:01:59 -0700

    mdl is used from webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import 'material-design-lite'
+
 console.log('Hello World from Webpacker')
+console.log('What?!')
